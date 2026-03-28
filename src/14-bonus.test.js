a62af2ca5ee3fe7abcbbd826df22872977f6ca42
Elodie S <elodie.sevestre86@gmail.com>
2026-03-28 13:17:59 +0100

    14-bonus.test.js continue test on initNextButton()

--- src/14-bonus.test.js
+++ src/14-bonus.test.js
@@ -1,7 +1,17 @@
-import { describe, test, expect } from "vitest";
+import { describe, test, expect, beforeEach } from "vitest";
 import { initNextButton } from "./14-bonus";
 
 describe("initNextButton", () => {
-  test("returns init next button", () => {});
-  expect(initNextButton()).toBe();
+  // beforeEach : fonction de Vitest qui s'exécute automatiquement avant chaque test.
+  // Ça permet de repartir d'un DOM propre à chaque fois pour que les tests ne s'influencent pas entre eux.
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <button id="next-question-button"></button>
+    `;
+  });
+  test('check if DOM id="next-question-button" exists', () => {
+    initNextButton();
+    const nextButton = document.getElementById("next-question-button");
+    expect(nextButton).not.toBe(null);
+  });
 });
