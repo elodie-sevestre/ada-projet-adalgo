40ef12d5479f40e0f7893dfbbb3499fcb19a1da9
Elodie S <elodie.sevestre86@gmail.com>
2026-03-28 11:02:45 +0100

    config jsdom et bonus.vitest.js

---
+++ src/14-bonus.js
@@ -0,0 +1,38 @@
+// Ã©crivez les tests attestant du bon fonctionnement des questions suivantes.
+// faites une phase de clean code et modifier certaines variables / noms de variable si nÃ©cessaire.
+
+function arraysEqual(a, b) {
+  return a.length === b.length && a.every((val, i) => val === b[i]);
+}
+
+// --- Affichage mauvaise rÃ©ponse avec la correction ---
+function badGirl(divID, data, nbQuestion) {
+  const div = document.getElementById(divID);
+  const question = data[nbQuestion - 1];
+  const correctAnswers = question.correctIndex
+    .map((index) => question.options[index])
+    .join(", ");
+  div.innerHTML = `
+        <div id="answerMessage">
+        <p>Mauvaise fille</p>
+        <p>La bonne rÃ©ponse Ã©tait :${correctAnswers}</p>
+        </div>
+        <button id="next-question">Question suivante</button>
+    `;
+}
+
+function initNextButton() {
+  const nextButton = document.getElementById("next-question-button");
+  const resultButton = document.createElement("button");
+  resultButton.innerText = "RÃ©sultat";
+  resultButton.classList.add("button", "hidden");
+  nextButton.classList.add("hidden");
+  resultButton.classList.add("hidden");
+  answerButtons.forEach((button) => (button.disabled = false));
+}
+
+// Fonction qui transforme le score en pourcentage
+
+function calculPourcentScore(finalScore, numberQuestions) {
+  pourcentScore = (finalScore / numberQuestions) * 100;
+}
