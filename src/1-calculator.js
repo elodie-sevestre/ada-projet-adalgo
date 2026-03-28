1cc3e8ad62beff722c498003ab9d340cf50ee8aa
Elodie S <elodie.sevestre86@gmail.com>
2026-03-28 16:30:31 +0100

    1-calculator mep

--- src/1-calculator.js
+++ src/1-calculator.js
@@ -1,20 +1,24 @@
+// ======================================= INSTRUCTION =======================================
+
 /*
-This function should return the result
-of an addition,
-subtraction,
-multiplication, or
-division
-
-depending on the operator provided.
-
+This function should return the result of an addition, subtraction, multiplication, or division.
 For example:
-
 const x = 2
 const y = 3
 const operator = '+'
-
 calculator(x, y, operator) => return 5
 */
+
+// ======================================= RESOLUTION ========================================
+
+/**
+ * Function for calculate
+ * @param {number} x
+ * @param {number} y
+ * @param {string} operator
+ * @returns result
+ */
+
 export const calculator = (x, y, operator) => {
   if (isNaN(x) || isNaN(y)) {
     return "is empty";
