346a2f172be913c445a687432093002834719baf
Elodie S <elodie.sevestre86@gmail.com>
2026-03-28 13:04:36 +0100

    14-bonus.test.js init test on initNextButton()

--- src/14-bonus.test.js
+++ src/14-bonus.test.js
@@ -1,2 +1,7 @@
 import { describe, test, expect } from "vitest";
 import { initNextButton } from "./14-bonus";
+
+describe("initNextButton", () => {
+  test("returns init next button", () => {});
+  expect(initNextButton()).toBe();
+});
